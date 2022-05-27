e339fce0d9e5d17c23546351f4a66d1e5bb3e599
username917 <86450720+username917@users.noreply.github.com>
2022-05-27 18:07:54 -0400

    Update ConditionsLoops.java

--- HelloWorld2/src/myFirstPackage/ConditionsLoops.java
+++ HelloWorld2/src/myFirstPackage/ConditionsLoops.java
@@ -1,12 +1,64 @@
 package myFirstPackage;
 import java.util.Scanner;
 
-public class ConditionsLoops {
+public class ConditionsAndLoops {
 	
-	public void StartMeUp () {
+	public void LoopMe () {
+		
+		//int i=0;
+		
+		//For loop with a break statement
+		for(int i=0; i<5; i++) {
+			
+			System.out.println("The number is: " + i);
+			
+			if(i==4) {
+				System.out.println("Breaking out of the loop at i=" + i + "!");
+				break;
+			}
+		}
+		
+		//for loop with a continue statement
+		for(int i=0; i<5; i++) {
+			
+			System.out.println("The number is: " + i);
+			
+			if(i==10) {
+				System.out.println("Continuing the loop at i=" + i + "!");
+			} else if (i ==3) {
+				System.out.println("Continuing the loop at i=" + i + "!");
+				continue;
+			} else if (i ==8) {
+				System.out.println("i is " + i);
+			}
+		}
 		
 		
+		//while loop
+		int j = 0;
 		
+		while(j<5) {
+			
+			System.out.println("My while loop number is: " + j);
+			j = j + 1; //j++
+			
+		}
+		
+		
+		//do-while loop
+		int k = 1;
+		
+		do {
+			
+			if(k == 8) {
+				System.out.println("My do-while number is: " + k);
+				break;
+			}
+			
+			k++;
+			
+			
+		} while (k<10);
 		
 	}
 	
