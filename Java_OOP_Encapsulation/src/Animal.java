ed1f3a4bfb9fa7b3451b554bb7f39f75ecef8ade
username917 <86450720+username917@users.noreply.github.com>
2022-07-05 22:08:02 -0400

    Add files via upload

---
+++ Java_OOP_Encapsulation/src/Animal.java
@@ -0,0 +1,32 @@
+
+public class Animal {
+	
+	private String name;
+	private double averageWeight;
+	private int numberOfLegs;
+	
+	
+	// Setter methods
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setAverageWeight(double averageWeight) {
+		this.averageWeight = averageWeight;
+	}
+	public void setNumberOfLegs(int numberOfLegs) {
+		this.numberOfLegs = numberOfLegs;
+	}
+	
+	// Getter methods
+	public String getName() {
+		return name;
+	}
+	public double getAverageWeight() {
+		return averageWeight;
+	}
+	public int getNumberOfLegs() {
+		return numberOfLegs;
+	}
+		
+
+}
