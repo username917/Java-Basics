39df8bc36e1a33d223ec1b881ab60039db78eca7
username917 <86450720+username917@users.noreply.github.com>
2022-07-05 22:06:27 -0400

    Add files via upload

---
+++ Java_OOP_Dynamic_Polymorphism/src/TestBicycle.java
@@ -0,0 +1,8 @@
+class TestBicycle {
+    public static void main(String[] args) {
+        Vehicle mVehicle = new Vehicle();
+        mVehicle.move();
+        Bicycle mBicycle = new Bicycle();
+        mBicycle.move();
+    }
+}
---
+++ Java_OOP_Dynamic_Polymorphism/src/Vehicle.java
@@ -0,0 +1,10 @@
+class Vehicle {
+    public void move() {
+        System.out.println("This vehicle moves by using petrol.");
+    }
+}
+class Bicycle extends Vehicle {
+    public void move() {
+        System.out.println("This bicycle moves by cycling.");
+    }
+}
