28955a60776627d3bdce2fe4f32cf9284f6e98bb
username917 <86450720+username917@users.noreply.github.com>
2022-07-15 14:14:58 -0400

    Add files via upload

---
+++ EmployeeProgram/src/EmployeeClasses/Vehicle.java
@@ -0,0 +1,23 @@
+package EmployeeClasses;
+
+public class Vehicle {
+	
+	String make = "";
+	String model = "";
+	
+	public String getMake() {
+		return make;
+	}
+	public void setMake(String make) {
+		this.make = make;
+	}
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	
+	
+
+}
